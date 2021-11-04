06a7213f548bdebbf99a2a548e67435160d66b0f
SyahrulRamadhn <syahrul19@mhs.akba.ac.id>
2021-11-04 17:50:34 +0800

    array

--- tugas-02-02/array_2.php
+++ tugas-02-02/array_2.php
@@ -5,47 +5,30 @@
     $Programmer_Python = array("Andi", "Fatma", "Fadli", "Haris", "Baco");
     
 
-  echo "Daftar mahasiswa yang masuk kelompok Programmer PHP dengan nama diurutkan secara Ascending Adalah ";
-  print_r($programmer_php);
- /*
-  sort($programmer_php, SORT_STRING);
-  foreach($programmer_php as $element){
-      echo str_pad($element, 9)." ";
-  }*/
-  echo "<br />"; echo "<br />";
-
-  echo "Daftar mahasiswa yang masuk kelompok Programmer Python dengan nama diurutkan secara Descending adalah ";
-  print_r(array_reverse($Programmer_Python));
-  /*rsort($Programmer_Python, SORT_STRING);
-  foreach($Programmer_Python as $element){
-      echo str_pad($element, 9)." ";
-  }*/
-  echo "<br />"; echo "<br />";
+ echo "Daftar mahasiswa yang masuk kelompok Programmer PHP dengan nama diurutkan secara Ascending Adalah "; 
+ sort($programmer_php, SORT_STRING);
+    foreach($programmer_php as $element){
+        echo str_pad($element, 9)." ";
+    }
+ echo "<br />"; echo "<br />";
   
- echo "Daftar mahasiswa yang masuk kelompok Programmer PHP "; echo "<br />";
- echo $programmer_php[5]. "<br />";
- echo $programmer_php[1]. "<br />";
- echo $programmer_php[3]. "<br />";
- echo $programmer_php[2]. "<br />";
+ echo "Daftar mahasiswa yang masuk kelompok Programmer Python dengan nama diurutkan secara Descending adalah ";
+ rsort($Programmer_Python, SORT_STRING);
+    foreach($Programmer_Python as $element){
+        echo str_pad($element, 9)." ";
+    }
+ echo "<br />"; echo "<br />";
+   
+ echo "Daftar mahasiswa yang masuk kelompok Programmer PHP "; 
+ print_r(array_diff($programmer_php, $Programmer_Python)); echo "<br />";
  echo "<br />";
- /*print_r(array_diff($programmer_php, $Programmer_Python)); echo "<br />";*/
 
- echo "Daftar mahasiswa yang masuk kelompok Programmer Python "; echo "<br />";
- echo $Programmer_Python[3]. "<br />";
- echo $Programmer_Python[2]. "<br />";
+ echo "Daftar mahasiswa yang masuk kelompok Programmer Python ";
+ print_r(array_diff($Programmer_Python,$programmer_php )); echo "<br />";
  echo "<br />";
- /*print_r(array_diff($Programmer_Python,$programmer_php )); echo "<br />";*/
 
  echo "Daftar mahasiswa yang masuk kelompok Programmer PHP dan ProgrammerPython "; echo "<br />";
- echo $programmer_php[0]. "<br />";
- echo $programmer_php[1]. "<br />";
- echo $programmer_php[2]. "<br />";
- echo $programmer_php[3]. "<br />";
- echo $programmer_php[4]. "<br />";
- echo $programmer_php[5]. "<br />";
- echo $programmer_php[6]. "<br />";
- echo $Programmer_Python[2]. "<br />";
- echo $Programmer_Python[3]. "<br />";
+ print_r(array_intersect($programmer_php, $Programmer_Python));
  echo "<br />";
 
  echo "Daftar seluruh mahasiswa yang masuk kelompok Programmer PHP atau
